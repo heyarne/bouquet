d8899644b4f40db6020a78063288bc192e0090df
Arne Schlüter <aesthaddicts@gmail.com>
2017-01-14 16:32:52 +0100

    Add route for detailed trip info

--- middleware/api/trips.js
+++ middleware/api/trips.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { requireLogin } = require('../session-utils')
 const { Trip } = require('../../models/trip')
+const { SearchResult } = require('../../models/search-result')
 
 router.post('/', requireLogin(), (req, res) => {
   const payload = {
@@ -26,8 +27,24 @@ router.post('/', requireLogin(), (req, res) => {
 
 router.get('/me', requireLogin(), (req, res) => {
   Trip.find({ _id: { $in: req.user.trips } })
+    // todo: find most recent results
     .then(trips => res.status(200).json(trips))
     .catch(({ message }) => res.status(500).json({ message }))
+})
+
+router.get('/:id', requireLogin(), (req, res) => {
+  if (req.user.trips.indexOf(req.params.id) === -1) {
+    res.status(403).json({ message: 'You\'re not allowed to access this' })
+  }
+
+  const { id } = req.params.id
+  const tripQuery = Trip.findOne(id)
+  const resultQuery = SearchResult.find({ tripQuery: id }).sort({ createdAt: -1 }).limit(30)
+  Promise.all([ tripQuery, resultQuery ])
+    .then(([ trip, searchResults ]) => {
+      trip.results = searchResults
+      res.status(200).json(trip)
+    })
 })
 
 module.exports = router
